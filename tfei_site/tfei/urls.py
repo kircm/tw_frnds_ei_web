5485324642696f635940a4b5ab6386afd05708cc
kircm <kircmarc@gmail.com>
2020-10-23 15:13:39 -0400

    Added some dummy views and a template structure

--- tfei_site/tfei/urls.py
+++ tfei_site/tfei/urls.py
@@ -7,6 +7,10 @@ favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
         re_path(r'^favicon\.ico$', favicon_view),
-        path('', views.index, name='index')
+        path('', views.index, name='index'),
+        path('main', views.main_v, name='main'),
+        path('export', views.export_v, name='export'),
+        path('import', views.import_v, name='import'),
+        path('logout', views.logout_v, name='logout')
 
         ]
--- tfei_site/tfei/views.py
+++ tfei_site/tfei/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
-from django.template import loader
+
 
 def index(request):
     context = {}
 
     return render(request, 'tfei/index.html', context)
+
+
+def main_v(request):
+    context = {}
+
+    return render(request, 'tfei/main.html', context)
+
+
+def export_v(request):
+    context = {}
+
+    return render(request, 'tfei/export.html', context)
+
+
+def import_v(request):
+    context = {}
+
+    return render(request, 'tfei/import.html', context)
+
+
+def logout_v(request):
+    context = {}
+
+    return render(request, 'tfei/logout.html', context)
 
