3af567af5fd1f692e3cd61bc9a5e3af3fb05403d
kircm <kircmarc@gmail.com>
2020-10-26 13:06:00 -0400

    Made all views class-based

--- tfei_site/tfei/urls.py
+++ tfei_site/tfei/urls.py
@@ -1,19 +1,23 @@
 from django.urls import path, re_path
 from django.views.generic.base import RedirectView
 
-from . import views
+from .views import ExportView
+from .views import ImportView
+from .views import IndexView
+from .views import LogoutView
+from .views import MainView
+from .views import TwAuthCallbackView
 from .views import TwAuthenticateRedirectView
 
 favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
-        re_path(r'^favicon\.ico$', favicon_view),
-        path('', views.index, name='index'),
-        path('main', views.main_v, name='main'),
-        path('tw_auth', TwAuthenticateRedirectView.as_view(), name='tw_auth'),
-        path('tw_auth_callback', views.tw_auth_callback, name='tw_auth_callback'),
-        path('export', views.export_v, name='export'),
-        path('import', views.import_v, name='import'),
-        path('logout', views.logout_v, name='logout')
-
-        ]
+    re_path(r'^favicon\.ico$', favicon_view),
+    path('', IndexView.as_view(), name='index'),
+    path('main', MainView.as_view(), name='main'),
+    path('export', ExportView.as_view(), name='export'),
+    path('import', ImportView.as_view(), name='import'),
+    path('logout', LogoutView.as_view(), name='logout'),
+    path('tw_auth', TwAuthenticateRedirectView.as_view(), name='tw_auth'),
+    path('tw_auth_callback', TwAuthCallbackView.as_view(), name='tw_auth_callback'),
+]
--- tfei_site/tfei/views.py
+++ tfei_site/tfei/views.py
@@ -1,29 +1,36 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.urls.base import reverse
+from django.views.generic.base import TemplateView, RedirectView
 from twython import Twython
+
 from .config_auth import APP_KEY
 from .config_auth import APP_SECRET
-from django.views.generic.base import RedirectView
-from django.urls.base import reverse
 
 oauth_store = {}
 
 
-def index(request):
-    context = {}
-
-    return render(request, 'tfei/index.html', context)
+class IndexView(TemplateView):
+    template_name = "tfei/index.html"
 
 
-def main_v(request):
-    context = {}
+class MainView(TemplateView):
+    template_name = "tfei/main.html"
 
-    return render(request, 'tfei/main.html', context)
+
+class ExportView(TemplateView):
+    template_name = "tfei/export.html"
+
+
+class ImportView(TemplateView):
+    template_name = "tfei/import.html"
+
+
+class LogoutView(TemplateView):
+    template_name = "tfei/logout.html"
 
 
 class TwAuthenticateRedirectView(RedirectView):
 
-    callback_url = ""  # To be set from a request object
+    callback_url = ""  # To be set once processing a request object
 
     def get(self, request, *args, **kwargs):
         self.callback_url = request.build_absolute_uri(reverse('tw_auth_callback'))
@@ -41,53 +48,55 @@ class TwAuthenticateRedirectView(RedirectView):
         return auth_url
 
 
-def tw_auth_callback(request):
-    if request.method == 'POST':
-        oauth_token = request.POST['oauth_token']
-        oauth_verifier = request.POST['oauth_verifier']
-        oauth_denied = request.POST.get('denied', False)
-    elif request.method == 'GET':
+class TwAuthCallbackView(TemplateView):
+
+    template_name = ""
+    context = {}
+
+    def get(self, request, *args, **kwargs):
         oauth_token = request.GET['oauth_token']
         oauth_verifier = request.GET['oauth_verifier']
         oauth_denied = request.GET.get('denied', False)
-    else:
-        return HttpResponse('Invalid method!')
+        self.template_name, self.context = self.process_oauth_callback(oauth_token, oauth_verifier, oauth_denied)
+        return super().get(request, *args, **kwargs)
 
-    if oauth_denied:
-        context = {'error_message': "the OAuth request was denied by this user"}
-        return render(request, 'tfei/auth_nk.html', context)
+    def post(self, request, *args, **kwargs):
+        oauth_token = request.POST['oauth_token']
+        oauth_verifier = request.POST['oauth_verifier']
+        oauth_denied = request.POST.get('denied', False)
+        self.template_name, self.context = self.process_oauth_callback(oauth_token, oauth_verifier, oauth_denied)
+        return super().post(request, *args, **kwargs)
 
-    if oauth_token not in oauth_store:
-        context = {'error_message': "oauth_token not found locally"}
-        return render(request, 'tfei/auth_nk.html', context)
+    def get_context_data(self, **kwargs):
+        return self.context
 
-    oauth_token_secret = oauth_store[oauth_token]
+    @staticmethod
+    def process_oauth_callback(oauth_token, oauth_verifier, oauth_denied):
+        if oauth_denied:
+            template_name = "tfei/auth_nk.html"
+            context = {'error_message': "the OAuth request was denied by this user"}
+            return template_name, context
 
-    twitter = Twython(APP_KEY, APP_SECRET, oauth_token, oauth_token_secret)
-    final_oauth_tokens = twitter.get_authorized_tokens(oauth_verifier)
+        if oauth_token not in oauth_store:
+            template_name = "tfei/auth_nk.html"
+            context = {'error_message': "oauth_token not found locally"}
+            return template_name, context
 
-    oauth_final_token = final_oauth_tokens['oauth_token']
-    oauth_final_token_secret = final_oauth_tokens['oauth_token_secret']
+        oauth_token_secret = oauth_store[oauth_token]
 
-    context = {'oauth_final_token': oauth_final_token,
-               'oauth_final_token_secret': oauth_final_token_secret}
+        twitter = Twython(APP_KEY, APP_SECRET, oauth_token, oauth_token_secret)
+        final_oauth_tokens = twitter.get_authorized_tokens(oauth_verifier)
+        oauth_final_token = final_oauth_tokens['oauth_token']
+        oauth_final_token_secret = final_oauth_tokens['oauth_token_secret']
 
-    return render(request, 'tfei/auth_ok.html', context)
+        creds = twitter.verify_credentials(skip_status=True,
+                                           include_entities=False,
+                                           include_email=False)
 
+        context = {
+            'oauth_final_token': oauth_final_token,
+            'oauth_final_token_secret': oauth_final_token_secret,
+            'user_screen_name': creds['screen_name']
+        }
 
-def export_v(request):
-    context = {}
-
-    return render(request, 'tfei/export.html', context)
-
-
-def import_v(request):
-    context = {}
-
-    return render(request, 'tfei/import.html', context)
-
-
-def logout_v(request):
-    context = {}
-
-    return render(request, 'tfei/logout.html', context)
+        return "tfei/auth_ok.html", context
