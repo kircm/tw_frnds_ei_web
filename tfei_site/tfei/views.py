f2e6341c65111abe3a4b1cc36ffe98b0118e63ec
kircm <kircmarc@gmail.com>
2020-10-29 09:17:25 -0400

    Use session to store tokens across views

--- tfei_site/tfei/views.py
+++ tfei_site/tfei/views.py
@@ -5,9 +5,6 @@ from twython import Twython
 from .config_auth import APP_KEY
 from .config_auth import APP_SECRET
 
-# OAUTH_store should be in session
-oauth_store = {}
-
 
 class IndexView(TemplateView):
     template_name = "tfei/index.html"
@@ -19,6 +16,16 @@ class MainView(TemplateView):
 
 class MainMenuView(TemplateView):
     template_name = "tfei/main-menu.html"
+
+    tw_context = {}
+
+    def get(self, request, *args, **kwargs):
+        self.tw_context = request.session['tw_context']
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = {'tw_context': self.tw_context}
+        return context
 
 
 class ExportView(TemplateView):
@@ -39,19 +46,26 @@ class ErrorView(TemplateView):
 
 class TwAuthenticateRedirectView(RedirectView):
 
-    callback_url = ""  # To be set once processing a request object
+    temp_oauth_store = {}
+    callback_url = None  # To be set once processing a request object
 
     def get(self, request, *args, **kwargs):
+        # Initialize oauth store in user's session
+        request.session['temp_oauth_store'] = self.temp_oauth_store
         self.callback_url = request.build_absolute_uri(reverse('tw_auth_callback'))
         return super().get(self, request, *args, **kwargs)
 
     def get_redirect_url(self, *args, **kwargs):
         twitter = Twython(app_key=APP_KEY, app_secret=APP_SECRET)
+
+        # TODO try catch - TEST Twitter responding error:
+        # Callback URL not approved for this client application.
+        # Approved callback URLs can be adjusted in your application settings
         auth = twitter.get_authentication_tokens(callback_url=self.callback_url, force_login=True)
 
         oauth_token = auth['oauth_token']
         oauth_token_secret = auth['oauth_token_secret']
-        oauth_store[oauth_token] = oauth_token_secret
+        self.temp_oauth_store[oauth_token] = oauth_token_secret
         auth_url = auth['auth_url']
 
         return auth_url
@@ -59,40 +73,51 @@ class TwAuthenticateRedirectView(RedirectView):
 
 class TwAuthCallbackView(RedirectView):
 
+    temp_oauth_store = {}
     absolute_url_builder = None
-    context = {}
     redirect_url = None
+    tw_context = {}
+    msg_context = {}
 
     def get(self, request, *args, **kwargs):
-        oauth_token = request.GET['oauth_token']
-        oauth_verifier = request.GET['oauth_verifier']
-        oauth_denied = request.GET.get('denied', False)
-        self.absolute_url_builder = request.build_absolute_uri
-        self.process_oauth_callback(oauth_token, oauth_verifier, oauth_denied)
+        self.process_request(request, request.GET)
         return super().get(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        oauth_token = request.POST['oauth_token']
-        oauth_verifier = request.POST['oauth_verifier']
-        oauth_denied = request.POST.get('denied', False)
+        self.process_request(request, request.POST)
+        return super().post(request, *args, **kwargs)
+
+    def process_request(self, request, req_method):
+        # extract temp_oauth_store and tw_context from user's session
+        self.temp_oauth_store = request.session['temp_oauth_store']
+        # Initialize tw_context in msg_context in session
+        request.session['tw_context'] = self.tw_context
+        request.session['msg_context'] = self.msg_context
+
+        oauth_token = req_method['oauth_token']
+        oauth_verifier = req_method['oauth_verifier']
+        oauth_denied = req_method.get('denied', False)
         self.absolute_url_builder = request.build_absolute_uri
         self.process_oauth_callback(oauth_token, oauth_verifier, oauth_denied)
-        return super().post(request, *args, **kwargs)
+        return
 
     def process_oauth_callback(self, oauth_token, oauth_verifier, oauth_denied):
         if oauth_denied:
-            self.context = {'error_message': "the OAuth request was denied by this user"}
+            self.msg_context = {'error_message': "the OAuth request was denied by this user"}
             self.redirect_url = self.absolute_url_builder(reverse("error_view"))
             return
 
-        if oauth_token not in oauth_store:
-            self.context = {'error_message': "oauth_token not found locally"}
+        if oauth_token not in self.oauth_store:
+            self.msg_context = {'error_message': "oauth_token not found locally"}
             self.redirect_url = self.absolute_url_builder(reverse("error_view"))
             return
 
-        oauth_token_secret = oauth_store[oauth_token]
+        # retrieve the token secret that had been kept in oauth store before redirecting to tw auth url
+        oauth_token_secret = self.oauth_store[oauth_token]
 
         twitter = Twython(APP_KEY, APP_SECRET, oauth_token, oauth_token_secret)
+
+        # try catch
         final_oauth_tokens = twitter.get_authorized_tokens(oauth_verifier)
         oauth_final_token = final_oauth_tokens['oauth_token']
         oauth_final_token_secret = final_oauth_tokens['oauth_token_secret']
@@ -102,7 +127,10 @@ class TwAuthCallbackView(RedirectView):
                                                          include_entities=False,
                                                          include_email=False)
 
-        self.context = {
+        # we don't need the temporary OAuth tokens anymore
+        del self.temp_oauth_store
+
+        self.tw_context = {
             'oauth_final_token': oauth_final_token,
             'oauth_final_token_secret': oauth_final_token_secret,
             'user_screen_name': creds['screen_name']
