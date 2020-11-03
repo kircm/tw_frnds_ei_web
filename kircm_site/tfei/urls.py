8697a086cc53491a190ab603b4711d1b128348e0
kircm <kircmarc@gmail.com>
2020-11-03 08:32:17 -0500

    Error message templating - CSS class changes

--- kircm_site/tfei/urls.py
+++ kircm_site/tfei/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 from .views import ErrorView
+from .views import IndexView
 from .views import LogoutView
 from .views import MainMenuView
 from .views import TwAuthCallbackView
 from .views import TwAuthenticateRedirectView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='tfei/index.html'), name='index'),
+    path('', IndexView.as_view(), name='index'),
     path('tw_auth', TwAuthenticateRedirectView.as_view(), name='tw_auth'),
     path('tw_auth_callback', TwAuthCallbackView.as_view(), name='tw_auth_callback'),
     path('mainmenu', MainMenuView.as_view(), name='main_menu'),
