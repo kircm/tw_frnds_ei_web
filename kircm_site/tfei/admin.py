898923eda30cc5f3d9f59f6df60569ff7947dba5
kircm <kircmarc@gmail.com>
2020-11-06 13:27:27 -0500

    Change model names - reset migrations - add tw user model

--- kircm_site/tfei/admin.py
+++ kircm_site/tfei/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import Permission
 
-from .models import TfeiTask
+from .models import Task
+from .models import TwUser
 
-admin.site.register(TfeiTask)
-
+admin.site.register(Task)
+admin.site.register(TwUser)
+admin.site.register(Permission)
--- kircm_site/tfei/models.py
+++ kircm_site/tfei/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 
 
-class TfeiTask(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class Task(models.Model):
+    tw_user = models.ForeignKey('TwUser', on_delete=models.PROTECT)
+    tw_screen_name_for_task = models.CharField(max_length=30)
 
-    tw_screen_name = models.CharField(max_length=30)
-    tw_id = models.BigIntegerField()
-
-    task_type = models.CharField(max_length=6)
+    task_type = models.CharField(max_length=20)
     task_status = models.CharField(max_length=20)
+    task_par_tw_id = models.BigIntegerField()
+    task_par_f_name = models.CharField(max_length=100)
 
     pending_at = models.DateTimeField(auto_now_add=True)
     running_at = models.DateTimeField(blank=True, null=True)
@@ -18,4 +17,16 @@ class TfeiTask(models.Model):
     finished_ok = models.BooleanField(null=True)
     finished_details = models.TextField(blank=True)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+
+class TwUser(models.Model):
+    tw_id = models.BigIntegerField(primary_key=True)
+    tw_screen_name = models.CharField(max_length=30)
+
+    tw_token = models.CharField(max_length=100)
+    tw_token_sec = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
