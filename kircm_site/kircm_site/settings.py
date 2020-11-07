4463fab478a3021e8d2b10c9803ee0fa7b4061eb
kircm <kircmarc@gmail.com>
2020-11-07 13:32:24 -0500

    Move DB back-end to MySQL

--- kircm_site/kircm_site/settings.py
+++ kircm_site/kircm_site/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = 'kircm_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': env('MYSQL_DB_HOST'),
+        'PORT': '3306',
+        'NAME': 'kircm$kircm_db',
+        'USER': 'kircm',
+        'PASSWORD': env('MYSQL_DB_PASSWORD'),
     }
 }
 
