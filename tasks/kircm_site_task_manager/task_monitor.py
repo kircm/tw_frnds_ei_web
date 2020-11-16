b11f9817b61d78242995b0869969d3d01a983085
kircm <kircmarc@gmail.com>
2020-11-16 08:00:32 -0500

    Refactored task monitor to use a decorator for operations the require locking/releasing the collection that holds futures for task threads

--- tasks/kircm_site_task_manager/task_monitor.py
+++ tasks/kircm_site_task_manager/task_monitor.py
@@ -1,3 +1,4 @@
+import functools
 import logging
 import threading
 import time
@@ -12,46 +13,77 @@ class TaskMonitor:
         self.__TASK_FUTURES = {}
         self.__TASK_FUTURES_LOCK = threading.Lock()
 
-    # TODO: Use locking decorator for synchronous methods
+    #
+    # lock/release decorator
+    #
+    # noinspection PyMethodParameters
+    def locks_task_futures_collection(func):
+        """
+        This decorator wraps functions that require a synchronous operation with task futures collection.
 
+        """
+
+        # noinspection PyTypeChecker, PyCallingNonCallable
+
+        # For operations that need to be run within a mutex locking __TASK_FUTURES
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            monitor_instance = args[0]
+            monitor_lock = monitor_instance.__TASK_FUTURES_LOCK
+            monitor_lock.acquire()
+            result = func(*args, **kwargs)
+            monitor_lock.release()
+            return result
+
+        return wrapper
+
+    @locks_task_futures_collection
     def exists_task_for_key(self, task):
         # key = task.tw_user_id
         # We use task id as key for now
         key = task.id
-
-        self.__TASK_FUTURES_LOCK.acquire()
         exists = key in self.__TASK_FUTURES
-        self.__TASK_FUTURES_LOCK.release()
-
         if exists:
             logger.warning(f"Task for key: {key} already existing in task monitor!")
         return exists
 
+    @locks_task_futures_collection
     def add_future(self, task, future):
         # key = task.tw_user_id
         # We use task id as key for now
         key = task.id
-
         logger.info(f"Adding future for task with key: {key}")
-        self.__TASK_FUTURES_LOCK.acquire()
         self.__TASK_FUTURES[key] = future
-        self.__TASK_FUTURES_LOCK.release()
 
+    @locks_task_futures_collection
     def all_finished(self):
         logger.debug("All futures finished?")
-        self.__TASK_FUTURES_LOCK.acquire()
         num_futures = len(self.__TASK_FUTURES) == 0
-        self.__TASK_FUTURES_LOCK.release()
         logger.debug(f"Returning num_futures: {num_futures}")
         return num_futures
 
+    @locks_task_futures_collection
     def num_unfinished(self):
         logger.debug("num unfinished?")
-        self.__TASK_FUTURES_LOCK.acquire()
         num_unfinished = len(self.__TASK_FUTURES)
-        self.__TASK_FUTURES_LOCK.release()
         logger.debug(f"Returning num unfinished: {num_unfinished}")
         return num_unfinished
+
+    @locks_task_futures_collection
+    def __remove_finished_futures(self):
+        # we need to lock access to the dict holding all
+        # futures for tasks while we do a pass to
+        # remove finished ones
+        futures_to_check = iter(self.__TASK_FUTURES.items())
+        f_item = next(futures_to_check, None)
+        while f_item:
+            if f_item[1].done():
+                self.__TASK_FUTURES.pop(f_item[0])
+                # we recreate iterator to refresh the current
+                # state of task futures dict - as we have popped an item
+                futures_to_check = iter(self.__TASK_FUTURES.items())
+            f_item = next(futures_to_check, None)
+        return len(self.__TASK_FUTURES)
 
     def monitor(self):
         threading.current_thread().name = "TaskMonitor"
@@ -61,13 +93,7 @@ class TaskMonitor:
 
         while unfinished:
             logger.info(f"Current unfinished task futures: {unfinished}")
-
-            # we need to lock access to the dict holding all
-            # futures for tasks while we do a pass to
-            # remove finished ones
-            self.__TASK_FUTURES_LOCK.acquire()
             unfinished = self.__remove_finished_futures()
-            self.__TASK_FUTURES_LOCK.release()
             logger.debug(f"After sweeping current task futures we have now {unfinished}")
 
             # we've looped through the task futures and removed the ones that were finished
@@ -79,19 +105,6 @@ class TaskMonitor:
         # All task futures finished - time to release resources
         logger.info(f"There are now {unfinished} task futures - EXITING MONITOR")
         return 0
-
-    # Needs to be run within a mutex locking __TASK_FUTURES
-    def __remove_finished_futures(self):
-        futures_to_check = iter(self.__TASK_FUTURES.items())
-        f_item = next(futures_to_check, None)
-        while f_item:
-            if f_item[1].done():
-                self.__TASK_FUTURES.pop(f_item[0])
-                # we recreate iterator to refresh the current
-                # state of task futures dict - as we have popped an item
-                futures_to_check = iter(self.__TASK_FUTURES.items())
-            f_item = next(futures_to_check, None)
-        return len(self.__TASK_FUTURES)
 
 
 class ExistingTaskForUser(Exception):
